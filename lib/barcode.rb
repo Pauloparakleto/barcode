322f1e9435f2f51eec34c9392c69594d3766c01b
Felipe <alparakleto@gmail.com>
2022-01-09 23:06:48 -0300

    feat(barcode.rb): add last_digit

--- lib/barcode.rb
+++ lib/barcode.rb
@@ -6,13 +6,22 @@ module Barcode
   class Error < StandardError; end
   # Your code goes here...
   class Barcode
-    attr_reader :incomplete_barcode_number
+    attr_accessor :incomplete_barcode_number
     def initialize(incomplete_barcode_number)
       @incomplete_barcode_number  = incomplete_barcode_number
     end
 
-    def last_number
+    def last_digit
+      normalized_digits = incomplete_barcode_number.to_s.split(//).map{|character| character.to_i} 
+      normalized_digits.collect!.with_index do |number, index|
+        if index.even?
+          number
+        else
+          number*3
+        end
+      end
 
+      10 - normalized_digits.sum%10
     end
   end
 end
